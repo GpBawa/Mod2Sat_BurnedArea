f4f0ba676ef16ca9e986acb855b955347dc47038
GpBawa <gurjeetpalbawa1990@gmail.com>
2021-07-27 13:04:41 +0530

    Update 1 - NBR_Diff.js

--- 1 - NBR_Diff.js
+++ 1 - NBR_Diff.js
@@ -1,28 +1,27 @@
-/**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var indiaShp = ee.FeatureCollection("projects/GlobalFires/IndiaAgFires/IND_adm1"),
-    modisNBR = ee.ImageCollection("projects/GlobalFires/IndiaAgFires/modisNBR"),
-    landsatNBR = ee.ImageCollection("projects/GlobalFires/IndiaAgFires/landsatNBR"),
-    glc30 = ee.Image("projects/GlobalFires/IndiaAgFires/glc30");
-/***** End of imports. If edited, may not auto-convert in the playground. *****/
+var indiaShp = ee.FeatureCollection("users/gurjeetpalbawa1990/projects/GlobalFires/IndiaAgFires/IND_adm1"),
+    igbpLandCoverVis = {"min":1,"max":17,"palette":["05450a","086a10","54a708","78d203","009900","c6b044","dcd159","dade48","fbff13","b6ff05","27ff87","c24f44","a5a5a5","ff6d4c","69fff8","f9ffa4","1c0dff"]},
+    glc30 = ee.Image("users/gurjeetpalbawa1990/projects/GlobalFires/IndiaAgFires/glc30"),
+    modisNBR = ee.Image("users/gurjeetpalbawa1990/projects/GlobalFires/IndiaAgFires/modisNBR/modisNBR_2016"),
+    sentinalNBR = ee.Image("users/gurjeetpalbawa1990/projects/GlobalFires/IndiaAgFires/sentinalNBR/sentinalNBR_2016");
+
+
 // ---------------------------------------------
-// Download MODIS-Landsat pre and post-fire NBR
+// Download MODIS-Sentinal pre and post-fire NBR
 // regionally averaged differences
 // ---------------------------------------------
-// Author: Tianjia Liu
-// Last updated: September 30, 2018
+// Author: Gurjeetpal
+// Last updated: July 07, 2021
 
 // Input Parameters:
-var params = require('users/tl2581/ModL2T_BA:InputParams.js');
+var params = require('users/gurjeetpalbawa1990/ModLand:InputParams.js');
 
 // Time Period
 var sYear = params.sYear; // Start Year
 var eYear = params.eYear; // End Year
 
 // State Boundaries
-var punjab = indiaShp.filterMetadata('STATE','equals','PUNJAB');
-var haryana = indiaShp.filterMetadata('STATE','equals','HARYANA');
-var states = haryana.merge(punjab);
 
+var states = indiaShp.filterMetadata('NAME_1','equals','Haryana');
 var Shp = states;
 
 // Pre-fire and post-fire Collection Dates
@@ -34,56 +33,58 @@ var getPtYr = function(feature) {
   return feature.setGeometry(ee.Geometry.Point(iYear,0));
 };
 
-var modisScale = ee.Image(modisNBR.first());
-var landScale = ee.Image(landsatNBR.first());
-var glc30Re = glc30.reproject({crs: landScale.projection(), scale: landScale.projection().nominalScale()});
+var modisScale = modisNBR;
+var sentinalScale = sentinalNBR;
 
-// ------------- START OF LOOP ---------------
+var glc30Re = glc30.clip(Shp);
+Map.addLayer(Shp,{},'Haryana');
+
 var nbrDiff = [];
-for(var iYear = sYear; iYear <= eYear; iYear++) {
-  
-  var modisNBRyr = modisNBR.filter(ee.Filter.calendarRange(iYear,iYear,'year')).first();
-  var modis_NBRpre = modisNBRyr.select('preFire');
-  var modis_NBRpost = modisNBRyr.select('postFire');
 
-  var landsatNBRyr = landsatNBR.filter(ee.Filter.calendarRange(iYear,iYear,'year')).first();
-  var landsat_NBRpre = landsatNBRyr.select('preFire');
-  var landsat_NBRpost = landsatNBRyr.select('postFire');
+var modis_NBRpre = modisNBR.select('preFire');
+var modis_NBRpost = modisNBR.select('postFire');
 
-  var pre_diff = modis_NBRpre.subtract(landsat_NBRpre).updateMask(glc30Re.eq(10));
-  var post_diff = modis_NBRpost.subtract(landsat_NBRpost).updateMask(glc30Re.eq(10));
+var sentinal_NBRpre = sentinalNBR.select('preFire');
+var sentinal_NBRpost = sentinalNBR.select('postFire');
 
-  var pre_regionDiff = pre_diff.reduceRegion({
-    geometry: Shp.geometry(),
-    reducer: ee.Reducer.mean(),
-    crs: modisScale.projection(),
-    scale: modisScale.projection().nominalScale(),
-    maxPixels: 1e12
-  }).toArray().multiply(1000).round().divide(1000).toList().get(0);
+var pre_diff = modis_NBRpre.subtract(sentinal_NBRpre).updateMask(glc30Re.eq(5));
+var post_diff = modis_NBRpost.subtract(sentinal_NBRpost).updateMask(glc30Re.eq(5));
 
-  var post_regionDiff = post_diff.reduceRegion({
-    geometry: Shp.geometry(),
-    reducer: ee.Reducer.mean(),
-    crs: modisScale.projection(),
-    scale: modisScale.projection().nominalScale(),
-    maxPixels: 1e12
-  }).toArray().multiply(1000).round().divide(1000).toList().get(0);
+Map.addLayer(pre_diff,{},"Pre_Diff");
+Map.addLayer(post_diff,{},"Post_Diff");
 
-  var nbrDiffYr = pre_diff.rename('preDiff')
-  .addBands(post_diff.rename('postDiff')).reduceRegions({
-    collection: Shp.geometry(),
-    reducer: ee.Reducer.mean(),
-    crs: modisScale.projection(),
-    scale: modisScale.projection().nominalScale(),
-  }).toList(500,0);
-  
-  var nbrDiff = ee.FeatureCollection(nbrDiff)
-    .merge(ee.FeatureCollection(nbrDiffYr).map(getPtYr));
-  
-  print(iYear.toString() + ' preCol & postCol diff:');
-  print(pre_regionDiff);
-  print(post_regionDiff);
-}
+
+var pre_regionDiff = pre_diff.reduceRegion({
+  geometry: Shp.geometry(),
+  reducer: ee.Reducer.mean(),
+  crs: modisScale.projection(),
+  scale: modisScale.projection().nominalScale(),
+  maxPixels: 1e12
+}).toArray().multiply(1000).round().divide(1000).toList().get(0);
+
+var post_regionDiff = post_diff.reduceRegion({
+  geometry: Shp.geometry(),
+  reducer: ee.Reducer.mean(),
+  crs: modisScale.projection(),
+  scale: modisScale.projection().nominalScale(),
+  maxPixels: 1e12
+}).toArray().multiply(1000).round().divide(1000).toList().get(0);
+
+var nbrDiffYr = pre_diff.rename('preDiff')
+.addBands(post_diff.rename('postDiff')).reduceRegions({
+  collection: Shp.geometry(),
+  reducer: ee.Reducer.mean(),
+  crs: modisScale.projection(),
+  scale: modisScale.projection().nominalScale(),
+}).toList(500,0);
+
+var nbrDiff = ee.FeatureCollection(nbrDiff)
+  .merge(ee.FeatureCollection(nbrDiffYr));
+
+// print(iYear.toString() + ' preCol & postCol diff:');
+print(pre_regionDiff);
+print(post_regionDiff);
+
 
 // Run tasks
 Export.table.toDrive({
