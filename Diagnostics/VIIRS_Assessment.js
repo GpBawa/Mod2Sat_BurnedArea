2fd0b68abbd84482d2c6b5b3d3287973da22a6dd
GpBawa <gurjeetpalbawa1990@gmail.com>
2021-08-05 15:14:06 +0530

    Update VIIRS_Assessment.js

--- Diagnostics/VIIRS_Assessment.js
+++ Diagnostics/VIIRS_Assessment.js
@@ -2,24 +2,24 @@ var mcd64a1 = ee.ImageCollection("MODIS/006/MCD64A1"),
     mcd12q1 = ee.ImageCollection("MODIS/006/MCD12Q1"),
     indiaShp = ee.FeatureCollection("users/gurjeetpalbawa1990/projects/GlobalFires/IndiaAgFires/IND_adm1"),
     mod2sat = ee.Image("users/gurjeetpalbawa1990/Mod2Sat_BA_2016");
-// -------------------------------------------------
+/// -------------------------------------------------
 // Download MODIS pre and post-fire NBR to estimate
 // two-tailed burned area thresholds
 // -------------------------------------------------
-// Author: Gurjeetpal Bawa
-// Last updated: July 27, 2021
+// Author: Gurjeetpal
+// Last updated: July 30, 2021
 
 // Input Parameters:
-var params = require('users/gurjeetpalbawa1990/ModLand:InputParams.js');
+var params = require('users/tl2581/ModL2T_BA:InputParams.js');
 
 // Time Period
 var sYear = 2016; // Start Year
 var eYear = params.eYear; // End Year
 
 // State Boundaries
-var punjab = indiaShp.filterMetadata('NAME_1','equals','Punjab');
+
 var haryana = indiaShp.filterMetadata('NAME_1','equals','Haryana');
-var states = haryana.merge(punjab).geometry();
+var states = haryana.geometry();
 
 var Shp = states;
 
@@ -34,18 +34,16 @@ var bufferPts = function(feature) {
 };
 
 // ------------- START OF LOOP ---------------
-var mcd64a1_Commission = []; var mod2sat_Commission = [];
-var mcd64a1_Omission = []; var mod2sat_Omission = [];
+var mod2sat_Commission = []; var mcd64a1_Commission = [];
+var mod2sat_Omission = []; var mcd64a1_Omission = [];
 for(var iYear = sYear; iYear <= eYear; iYear++) {
   
   var filterYr = ee.Filter.calendarRange(iYear,iYear,'year');
   var filterMon = ee.Filter.calendarRange(10,11,'month');
   
-  
-  // Mod2Sat BA
-  var mod2satYr = ee.Image(mod2sat.clip(Shp)).gt(1).selfMask();
-  Map.addLayer(mod2satYr,{palette:"blue"}, "MOD2SAT")
-  
+  // ModL2T BA
+  var mod2satYr = ee.Image(mod2sat).gt(1).selfMask();
+
   // MCD12Q1 C6 agricultural mask
   var mcd12q1Yr = ee.Image(mcd12q1.filter(filterYr).first())
     .select('LC_Type2').eq(12);
@@ -54,8 +52,6 @@ for(var iYear = sYear; iYear <= eYear; iYear++) {
   var mcd64a1Yr = ee.Image(mcd64a1.filter(filterYr).filter(filterMon).select('BurnDate').max())
     .gt(0).selfMask()
     .reproject({crs: modisScale.projection(), scale: modisScale.projection().nominalScale()});
-    
-  Map.addLayer(mcd64a1Yr,{palette:"Red"}, "MCD64A1")
   
   // VIIRS active fires
   var viirsOct = ee.FeatureCollection('projects/GlobalFires/VNP14IMGML/VNP14IMGML_' +
@@ -64,12 +60,9 @@ for(var iYear = sYear; iYear <= eYear; iYear++) {
       iYear + '_11');
   
   var viirsPts = viirsOct.merge(viirsNov).filterMetadata('type','equals',0)
-    // .filterBounds(Shp).map(bufferPts);
-    .filterBounds(Shp);
-    
-  print("VIIRS Total:",viirsPts.size())
-  
-   var mod2satYr_Omission = mod2satYr.reduceRegions({
+    .filterBounds(Shp).map(bufferPts);
+
+  var mod2satYr_Omission = mod2satYr.reduceRegions({
     collection: viirsPts,
     reducer: ee.Reducer.count().unweighted(),
     crs: 'EPSG:4326',
@@ -91,8 +84,6 @@ for(var iYear = sYear; iYear <= eYear; iYear++) {
       crs: 'EPSG:4326',
       scale: 10
     }).toList(1,0).get(0);
-  print(mod2satYr_Commission)
-  
     
   var mcd64a1Yr_Commission = mcd64a1Yr.rename('MCD64A1')
     .addBands(mcd64a1Yr.clip(viirsPts).rename('MCD64A1_Agree'))
@@ -102,9 +93,8 @@ for(var iYear = sYear; iYear <= eYear; iYear++) {
       crs: modisScale.projection(),
       scale: modisScale.projection().nominalScale()
     }).toList(1,0).get(0);
-  print(mcd64a1Yr_Commission)
   
-  mod2sat_Omission = ee.FeatureCollection(mod2sat_Omission).merge(mod2satYr_Omission);
+  mod2sat_Omission = ee.FeatureCollection(mod2satYr_Omission).merge(mod2satYr_Omission);
   mcd64a1_Omission = ee.FeatureCollection(mcd64a1_Omission).merge(mcd64a1Yr_Omission);
   
   mod2sat_Commission[(iYear-sYear)] = ee.Feature(mod2satYr_Commission).setGeometry(ee.Geometry.Point(iYear,0));
@@ -126,7 +116,6 @@ Export.table.toDrive({
   description: 'Mod2Sat_Omission',
   selectors: ['YYYYMMDD','HHMM','conf','count']
 });
-
 
 Export.table.toDrive({
   collection: ee.FeatureCollection(mcd64a1_Omission),
